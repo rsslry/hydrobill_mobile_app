53ec1303f36a53ad4ed0110ad025f4915cced50d
Russel Rey Lupian <russelrey.lupian@bisu.edu.ph>
2023-12-09 14:01:41 +0800

    Commit

--- app/src/main/java/com/myfinalproject/hydrobill/readings_activity.java
+++ app/src/main/java/com/myfinalproject/hydrobill/readings_activity.java
@@ -3,23 +3,37 @@ package com.myfinalproject.hydrobill;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class readings_activity extends AppCompatActivity {
 
+    Button button;
+    Dialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_readings);
 
+        button = findViewById(R.id.search_consumer);
+        dialog = new Dialog(this);
         View rootView = findViewById(android.R.id.content);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog();
+            }
+        });
         // Fade-in animation
         AlphaAnimation fadeIn = new AlphaAnimation(0, 1);
         fadeIn.setDuration(500); // 1 second
@@ -59,5 +73,20 @@ public class readings_activity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+    private void showCustomDialog() {
+        dialog.setContentView(R.layout.popup_new_barangay_added_successfully);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button backButtonInDialog = dialog.findViewById(R.id.backbtndialog);
+        backButtonInDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle back button click in the dialog
+                dialog.dismiss(); // Dismiss the dialog if needed
+            }
+        });
+
+        dialog.show();
     }
 }
